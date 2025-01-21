0ce11a902045dea517cda36a31858c06ab78670a
gengyuanmax <gengyuanmax@gmail.com>
2025-01-21 14:41:04 +0100

    a100_llama

--- openeqa/baselines/llama.py
+++ openeqa/baselines/llama.py
@@ -7,11 +7,13 @@ import argparse
 import json
 from pathlib import Path
 from typing import Optional
-
+import os
 import tqdm
 
 from openeqa.utils.llama_utils import LLaMARunner, enable_full_determinism
 from openeqa.utils.prompt_utils import load_prompt
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 
 def parse_args() -> argparse.Namespace:
